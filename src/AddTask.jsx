c9f7972aae649a06cb18dbd2bc24760f4347e7f2
dgonzalez777 <1989.daniela@gmail.com>
2024-02-09 20:47:58 -0400

    Add context and reducer to todoList

---
+++ src/AddTask.jsx
@@ -0,0 +1,30 @@
+import { useState, useContext } from "react";
+import { useTasksDispatch } from "./TasksContext.jsx";
+
+export default function AddTask({ onAddTask }) {
+  const [text, setText] = useState("");
+  const dispatch = useTasksDispatch();
+  return (
+    <>
+      <input
+        placeholder="Agregar tarea"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          setText("");
+          dispatch({
+            type: "added",
+            id: nextId++,
+            text: text,
+          });
+        }}
+      >
+        Agregar
+      </button>
+    </>
+  );
+}
+
+let nextId = 3;
