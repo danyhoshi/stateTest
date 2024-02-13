48bcf584d6705d0bd9abd8a6f05ead46332b21da
dgonzalez777 <1989.daniela@gmail.com>
2024-02-12 22:49:05 -0400

    Add key to preserve state

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,44 @@ import Letter from "./Letter.jsx";
 import "./App.css";
 // Cada vez que se hace clic en el botón, el estado de la entrada desaparece. Esto se debe a que se crea una función diferente de MyTextField para cada renderizado de MyComponent. Estás renderizando un componente diferente en la misma posición, por lo que React reinicia todo el estado que esté anidado por debajo. Esto conlleva a errores y problemas de rendimiento. Para evitar este problema, declara siempre las funciones del componente en el nivel superior, y no anides sus definiciones.
 export default function App() {
-  const [counter, setCounter] = useState(0);
+  const [isPlayerA, setIsPlayerA] = useState(true);
+  return (
+    <div>
+      {isPlayerA ? (
+        <Counter key="kgjf" person="Taylor" />
+      ) : (
+        <Counter key="oyoy" person="Sarah" />
+      )}
+      <button
+        onClick={() => {
+          setIsPlayerA(!isPlayerA);
+        }}
+      >
+        ¡Siguiente jugador!
+      </button>
+    </div>
+  );
+}
 
-  function MyTextField() {
-    const [text, setText] = useState("");
+function Counter({ person }) {
+  const [score, setScore] = useState(0);
+  const [hover, setHover] = useState(false);
 
-    return <input value={text} onChange={(e) => setText(e.target.value)} />;
+  let className = "counter";
+  if (hover) {
+    className += " hover";
   }
 
   return (
-    <>
-      <MyTextField />
-      <button
-        onClick={() => {
-          setCounter(counter + 1);
-        }}
-      >
-        Hiciste clic {counter} veces
-      </button>
-    </>
+    <div
+      className={className}
+      onPointerEnter={() => setHover(true)}
+      onPointerLeave={() => setHover(false)}
+    >
+      <h1>
+        Puntos de {person}: {score}
+      </h1>
+      <button onClick={() => setScore(score + 1)}>Agregar uno</button>
+    </div>
   );
 }
