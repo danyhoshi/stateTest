9af02cf2bda222266080e5915d2b4c445518cae4
dgonzalez777 <1989.daniela@gmail.com>
2024-02-13 21:14:58 -0400

    Context in intermediate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,42 @@
 import Heading from "./Heading";
 import Section from "./Section";
 
-export default function Page() {
+export default function ProfilePage() {
   return (
     <Section>
-      <Heading>Título</Heading>
-      <Section>
-        <Heading>Encabezado</Heading>
-        <Heading>Encabezado</Heading>
-        <Heading>Encabezado</Heading>
-        <Section>
-          <Heading>Sub-encabezado</Heading>
-          <Heading>Sub-encabezado</Heading>
-          <Heading>Sub-encabezado</Heading>
-          <Section>
-            <Heading>Sub-sub-encabezado</Heading>
-            <Heading>Sub-sub-encabezado</Heading>
-            <Heading>Sub-sub-encabezado</Heading>
-          </Section>
-        </Section>
-      </Section>
+      <Heading>Mi perfil</Heading>
+      <Post title="¡Hola viajero!" body="Lee sobre mis aventuras." />
+      <AllPosts />
+    </Section>
+  );
+}
+
+function AllPosts() {
+  return (
+    <Section>
+      <Heading>Publicaciones</Heading>
+      <RecentPosts />
+    </Section>
+  );
+}
+
+function RecentPosts() {
+  return (
+    <Section>
+      <Heading>Publicaciones recientes</Heading>
+      <Post title="Sabores de Lisboa" body="¡...esos pastéis de nata!" />
+      <Post title="Buenos Aires a ritmo de tango" body="¡Me encantó!" />
+    </Section>
+  );
+}
+
+function Post({ title, body }) {
+  return (
+    <Section isFancy={true}>
+      <Heading>{title}</Heading>
+      <p>
+        <i>{body}</i>
+      </p>
     </Section>
   );
 }
