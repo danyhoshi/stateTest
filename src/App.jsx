2922d2d1dfe3677dbc7e1e8c4edb882e6ff57b3c
dgonzalez777 <1989.daniela@gmail.com>
2024-02-14 22:32:48 -0400

    UseRef clearTimeout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState, useRef } from "react";
+import { useRef, useState } from "react";
 
-export default function Stopwatch() {
-  const [startTime, setStartTime] = useState(null);
-  const [now, setNow] = useState(null);
-  const intervalRef = useRef(null);
+export default function Chat() {
+  const [text, setText] = useState("");
+  const [isSending, setIsSending] = useState(false);
+  const timeoutID = useRef(null);
 
-  function handleStart() {
-    setStartTime(Date.now());
-    setNow(Date.now());
-
-    clearInterval(intervalRef.current);
-    // console.log(intervalRef.current);
-    intervalRef.current = setInterval(() => {
-      setNow(Date.now());
-    }, 10);
+  function handleSend() {
+    setIsSending(true);
+    timeoutID.current = setTimeout(() => {
+      alert("¡Enviado!");
+      setIsSending(false);
+    }, 3000);
   }
 
-  function handleStop() {
-    clearInterval(intervalRef.current);
-  }
-
-  let secondsPassed = 0;
-  if (startTime != null && now != null) {
-    secondsPassed = (now - startTime) / 1000;
+  function handleUndo() {
+    setIsSending(false);
+    clearTimeout(timeoutID.current);
   }
 
   return (
     <>
-      <h1>Tiempo transcurrido: {secondsPassed.toFixed(3)}</h1>
-      <button onClick={handleStart}>Iniciar</button>
-      <button onClick={handleStop}>Detener</button>
+      <input
+        disabled={isSending}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button disabled={isSending} onClick={handleSend}>
+        {isSending ? "Enviando..." : "Enviar"}
+      </button>
+      {isSending && <button onClick={handleUndo}>Deshacer</button>}
     </>
   );
 }
